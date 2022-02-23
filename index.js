30817ac98884be8e83f1d99bd4776ec0a8faa431
matheus <matheusfelippeh@gmail.com>
2022-02-23 17:17:05 -0300

    'feat: add routes to /products and /sales'

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
+const express = require('express');
 
+const { productsRouter, salesRouter } = require('./routes/index');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/products', productsRouter);
+
+app.use('/products', salesRouter);
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const products = require('./products');
+const sales = require('./sales');
+
+module.exports = {
+  productsRouter: products,
+  salesRouter: sales,
+};
