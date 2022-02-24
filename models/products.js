7e981811eb68d5c1c84f4d552aa3828c9532e731
matheus <matheusfelippeh@gmail.com>
2022-02-24 04:47:34 -0300

    'fix: remove console.log'

--- models/products.js
+++ models/products.js
@@ -3,7 +3,6 @@ const DB = require('./connection');
 const getAll = async () => {
   const query = 'SELECT * FROM StoreManager.products ORDER BY id';
   const [result] = await DB.execute(query);
-  console.log(result);
   return result;
 };
 
