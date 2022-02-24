76ec8563f8529d0d320764374a74f567d75eed34
matheus <matheusfelippeh@gmail.com>
2022-02-24 09:42:24 -0300

    'feat: add service for sales getById + tests'

--- services/sales.js
+++ services/sales.js
@@ -11,6 +11,20 @@ const getAll = async () => {
   return { code: 200, data };
 };
 
+const getById = async (id) => {
+  const [modelResponse] = await salesModel.getById(id);
+  if (!modelResponse || modelResponse.length === 0) return { code: 404, message: 'Sale not found' };
+  const data = {
+    saleId: modelResponse.sale_id,
+    productId: modelResponse.product_id,
+    quantity: modelResponse.quantity,
+    date: modelResponse.date,
+  };
+
+  return { code: 200, data };
+};
+
 module.exports = {
   getAll,
+  getById,
 };
