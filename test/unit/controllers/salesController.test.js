96066d822f017738ac139093c32957d0d74e6fe4
matheus <matheusfelippeh@gmail.com>
2022-02-24 12:40:12 -0300

    'feat: add controller for sales getById + tests'

--- test/unit/controllers/salesController.test.js
+++ test/unit/controllers/salesController.test.js
@@ -36,4 +36,44 @@ describe('Controller Tests: ',()=>{
 
     })
   })
+  describe('List sale by id',()=>{
+    describe('If the sale is found', ()=>{
+      const serviceResponse = {code: 200, data: {id: 1, date: '2021-09-09  00:45:32'}}
+      request.params = { id: 1 }
+      before(()=>{
+        sinon.stub(salesService, 'getById').resolves(serviceResponse)
+      })
+      after(() => {
+        salesService.getById.restore();
+      });
+      it('Calls response.status with 200 value from serviceResponse.', async ()=>{
+        await salesController.getById(request, response, next);
+        expect(response.status.calledWith(serviceResponse.code)).to.be.true;
+      })
+
+      it('Calls response.json with data from serviceResponse.', async()=> {
+        await salesController.getById(request, response, next);
+        expect(response.json.calledWith(serviceResponse.data)).to.be.true;
+      })
+    })
+    describe('If the sale is not found', ()=>{
+      const serviceResponse = { code: 404, message: 'Sale not found'}
+      request.params = { id: 1 }
+      before(()=>{
+        sinon.stub(salesService, 'getById').resolves(serviceResponse)
+      })
+      after(() => {
+        salesService.getById.restore();
+      });
+      it('Calls response.status with 404 value from serviceResponse.', async ()=>{
+        await salesController.getById(request, response, next);
+        expect(response.status.calledWith(serviceResponse.code)).to.be.true;
+      })
+
+      it('Calls response.json with message from serviceResponse.', async()=> {
+        await salesController.getById(request, response, next);
+        expect(response.json.calledWith({message: serviceResponse.message})).to.be.true;
+      })
+    })
+  })
 })
